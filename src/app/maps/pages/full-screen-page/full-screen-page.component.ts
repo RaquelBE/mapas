cc66fa04949ace1e76a24bbda92f4ea73dfe129d
Raquel Blazquez <raquel.blazquez@bosonit.com>
2023-09-22 09:35:49 +0200

    instalar y mostrar mapa en pantalla completa

--- src/app/maps/pages/full-screen-page/full-screen-page.component.ts
+++ src/app/maps/pages/full-screen-page/full-screen-page.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
+
+(mapboxgl as any).accessToken =
+  'pk.eyJ1IjoicmFxdWVsMjI5MiIsImEiOiJjbG10ODFteWYwMzA0MndxcGpsNW9rd21vIn0.t1wyjbjuYEA-ZHWZs44PIg';
 
 @Component({
   selector: 'full-screen-page',
   templateUrl: './full-screen-page.component.html',
-  styleUrls: ['./full-screen-page.component.css']
+  styleUrls: ['./full-screen-page.component.css'],
 })
-export class FullScreenPageComponent {
-
+export class FullScreenPageComponent implements AfterViewInit {
+  ngAfterViewInit(): void {
+    const map = new mapboxgl.Map({
+      container: 'map', // container ID
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: [-74.5, 40], // starting position [lng, lat]
+      zoom: 9, // starting zoom
+    });
+  }
 }
