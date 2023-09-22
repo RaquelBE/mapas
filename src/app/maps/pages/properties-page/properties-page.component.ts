c5d842ecd0cfb406b2b9c6686573f5fc091eddf5
Raquel Blazquez <raquel.blazquez@bosonit.com>
2023-09-22 13:36:17 +0200

    creacción de standalone como página

--- src/app/maps/pages/properties-page/properties-page.component.ts
+++ src/app/maps/pages/properties-page/properties-page.component.ts
@@ -24,14 +24,14 @@ export class PropertiesPageComponent {
       lngLat: [-5.25465642995016, 39.87296673002342],
     },
     {
-      title: 'Luna de Miel, México',
-      description: 'Viaje de Ensueño, Cancún',
+      title: 'Cancún, México',
+      description: 'Luna de Miel, Cancún',
       lngLat: [-86.82851552553302, 21.178571084686084],
     },
     {
       title: 'Islas Mujeres, México',
       description: 'Playa Transparente, México',
-      lngLat: [-86.70972994877005, 21.231593122227466],
+      lngLat: [-86.70070422142417, 21.24551962896932],
     },
   ];
 }
