c8d83176da1bff7f0948103e86591ea622730fc3
Raquel Blazquez <raquel.blazquez@bosonit.com>
2023-09-22 12:30:18 +0200

    modificación de houses, personalizadas

--- src/app/maps/pages/properties-page/properties-page.component.ts
+++ src/app/maps/pages/properties-page/properties-page.component.ts
@@ -9,29 +9,29 @@ interface House {
 @Component({
   selector: 'properties-page',
   templateUrl: './properties-page.component.html',
-  styleUrls: ['./properties-page.component.css']
+  styleUrls: ['./properties-page.component.css'],
 })
 export class PropertiesPageComponent {
   public houses: House[] = [
     {
-      title: 'Casa residencial, Canadá',
-      description: 'Bella propiedad en Katana, Canadá',
-      lngLat: [ -75.92722289474008, 45.280015511264466]
+      title: 'Casa residencial, Seseña',
+      description: 'Nuestro Hogar, Toledo',
+      lngLat: [-3.6915409818313947, 40.11006491539797],
     },
     {
-      title: 'Casa de playa, México',
-      description: 'Hermosa casa de playa en Acapulco, México',
-      lngLat: [ -99.91287720907991, 16.828940930185748]
+      title: 'Casa del Pueblo, Caleruela',
+      description: 'Donde crecí, Toledo',
+      lngLat: [-5.25465642995016, 39.87296673002342],
     },
     {
-      title: 'Apartamento, Argentina',
-      description: 'Lujoso apartamento en el corazón de Buenos Aires, Argentina',
-      lngLat: [ -58.430166677283445, -34.57150108832866 ]
+      title: 'Luna de Miel, México',
+      description: 'Viaje de Ensueño, Cancún',
+      lngLat: [-86.82851552553302, 21.178571084686084],
     },
     {
-      title: 'Local comercial, España',
-      description: 'Local comercial disponible en Madrid, España, cerca de El Jardín Secreto.',
-      lngLat: [ -3.7112735618380177, 40.42567285425766 ]
+      title: 'Islas Mujeres, México',
+      description: 'Playa Transparente, México',
+      lngLat: [-86.70972994877005, 21.231593122227466],
     },
-  ]
+  ];
 }
