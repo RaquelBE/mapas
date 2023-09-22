eb0b2e56f43e45fd8ac1d2ae84173a87d3e10bce
Raquel Blazquez <raquel.blazquez@bosonit.com>
2023-09-22 14:16:56 +0200

    componente tradicional a un Standalone y final proyecto

--- src/app/alone/components/counter-alone/counter-alone.component.ts
+++ src/app/alone/components/counter-alone/counter-alone.component.ts
@@ -1,12 +1,10 @@
 import { Component, Input } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'counter-alone',
   standalone: true,
-  //imports: [CommonModule],
   templateUrl: './counter-alone.component.html',
-  styleUrls: ['./counter-alone.component.css']
+  styleUrls: ['./counter-alone.component.css'],
 })
 export class CounterAloneComponent {
   @Input()
